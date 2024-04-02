109ec8e057624f03e715eedc964d2b8155f0a82a
guisfons <guilhermesfonsecaa@gmail.com>
2024-04-02 17:02:51 -0300

    Base project

---
+++ html/assets/js/main.js
@@ -0,0 +1,54 @@
+$( document ).ready(function () {
+    if($('.loader').length) {
+        $('.loader').addClass('loader--hide')
+        setTimeout(function() {
+            $('.loader').remove()
+        }, 3999)
+    }
+
+    $('img').each(function() {
+        $(this).attr('draggable', false)
+    })
+
+    map()
+})
+
+function map () {
+    mapCount()
+    mapNavigation()
+}
+
+function mapCount () {
+    const maps = $('.maps__item')
+}
+
+function mapNavigation () {
+    $(document).on('click', '.maps__item:not(.maps__item--active), .maps__nav-next, .maps__nav-prev', function() {
+        let selectedEl = $(this)
+        let activeMap, selectedMap
+        if(selectedEl.hasClass('maps__nav-next')) {
+            selectedMap = selectedEl.closest('.maps').find('.maps__selection .maps__item--active').next()
+            activeMap = selectedEl.closest('.maps').find('.maps__selection .maps__item--active')
+        } else if (selectedEl.hasClass('maps__item')){
+            selectedMap = selectedEl
+            activeMap = selectedMap.prev('.maps__item--active')
+        } else if (selectedEl.hasClass('maps__nav-prev')) {
+            selectedMap = selectedEl.closest('.maps').find('.maps__selection .maps__item').last()
+            activeMap = selectedMap.parent().find('.maps__item--active')
+        }
+
+        if(selectedMap.prev().hasClass('maps__item--active')) {
+            selectedMap.addClass('maps__item--active')
+            activeMap.addClass('maps__item--swap')
+            $('.maps__item--swap').fadeOut('slow', function() {
+                activeMap.removeClass('maps__item--active').removeClass('maps__item--swap').remove()
+                $('.maps__selection').append(activeMap.removeClass('maps__item--active'))
+                $(this).css('display', '')
+            })
+        } else {
+            activeMap.removeClass('maps__item--active')
+            selectedMap.remove()
+            $('.maps__selection').prepend(selectedMap.addClass('maps__item--active'))
+        }
+    })
+}
